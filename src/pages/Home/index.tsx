ffb3eb6ff80918274e60b22fc44be4978f32b2b0
rubenskj <rubenskleinschmidtjr@gmail.com>
2021-05-17 19:56:21 -0300

    Add simple butto

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
 
 import './styles.css';
 
 const Home: React.FC = () => {
+  const [count, setCount] = useState<number>(0);
+
+  function incrementCount(): void {
+    setCount(count + 1);
+  }
+
   return (
     <div className="container">
       <div className="card">
         <h1>Hello dear!!</h1>
         <p>Hotjar has been configured!!</p>
-        <button>Test a Cool Button for <strong>Heatmap</strong></button>
+        {count && (
+          <React.Fragment>
+            <p>Count will be shown in HotJar</p>
+            <p className="count">{count}</p>
+          </React.Fragment>
+        )}
+        <button onClick={incrementCount}>Test a Cool Button for <strong>Heatmap</strong></button>
       </div>
     </div>
   );
